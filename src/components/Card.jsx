924ca34f055cc9e4f9998ab577f5054e852a0676
farkhanmhd <farkhanmuhammadv3@gmail.com>
2023-11-26 23:09:10 +0700

    added footer

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,9 +4,9 @@ export default function Card({ imageSource, children }) {
       <img
         src={imageSource}
         alt={"image"}
-        className="w-full h-auto grayscale-[.4] group-hover:grayscale-0 duration-300"
+        className="w-full h-auto grayscale-[.4] group-hover:grayscale-0 duration-300 group-hover:scale-110"
       />
-      <p className=" px-5 pb-2 leading-8 absolute bottom-0 opacity-0 group-hover:opacity-100 bg-gradient-to-b from-transparent to-slate-900 text-white duration-300 line-clamp-2 text-ellipsis">
+      <p className=" px-5 pb-2 leading-7 absolute bottom-0 opacity-0 group-hover:opacity-100 bg-gradient-to-b from-transparent to-slate-900 text-white duration-300 line-clamp-2 text-ellipsis">
         {children}
       </p>
     </div>
---
+++ src/components/Gallery.jsx
@@ -0,0 +1,125 @@
+import Card from "./Card";
+
+const Gallery = () => {
+  return (
+    <div className=" gallery max-w-7xl columns-1 sm:columns-2 md:columns-4 lg:columns-5 gap-5 mt-14">
+      <Card imageSource={"./images/1.jpg"}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum vitae iste
+        repudiandae asperiores dicta iusto consequuntur animi,
+      </Card>
+      <Card imageSource={"./images/2.jpg"}>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio,
+        perferendis?
+      </Card>
+      <Card imageSource={"./images/3.jpg"}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Et dicta,
+        dignissimos laudantium nesciunt amet nisi?
+      </Card>
+      <Card imageSource={"./images/4.jpg"}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto
+        vitae tempora eligendi voluptatem, assumenda reiciendis nobis quae
+        facere delectus in?
+      </Card>
+      <Card imageSource={"./images/5.jpg"}>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Porro,
+        reprehenderit!
+      </Card>
+      <Card imageSource={"./images/6.jpg"}>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatum
+        reiciendis qui illum unde sit perspiciatis.
+      </Card>
+      <Card imageSource={"./images/7.jpg"}>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Architecto
+        temporibus, minima ut accusantium qui ex necessitatibus. Nostrum
+        provident aliquam voluptatem.
+      </Card>
+      <Card imageSource={"./images/8.jpg"}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate
+        harum corporis similique illum nesciunt aperiam.
+      </Card>
+      <Card imageSource={"./images/9.jpg"}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum,
+        aliquam.
+      </Card>
+      <Card imageSource={"./images/10.jpg"}>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatibus
+        ipsa aperiam veritatis alias magnam itaque, nam dolor, suscipit illum
+        aspernatur quidem, neque dolorem est modi.
+      </Card>
+      <Card imageSource={"./images/11.jpg"}>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusamus
+        quisquam atque recusandae in!
+      </Card>
+      <Card imageSource={"./images/12.jpg"}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis, dicta
+        voluptatibus nihil eos ratione quis mollitia! Sit.
+      </Card>
+      <Card imageSource={"./images/13.jpg"}>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Commodi ipsum,
+        maiores eos consectetur ab debitis ratione quaerat facere.
+      </Card>
+      <Card imageSource={"./images/14.jpg"}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit
+        distinctio maxime ea, reiciendis, deserunt fuga, expedita voluptas atque
+        iure ipsum ut vitae!
+      </Card>
+      <Card imageSource={"./images/15.jpg"}>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat aut
+        atque reprehenderit obcaecati officiis!
+      </Card>
+      <Card imageSource={"./images/16.jpg"}>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deleniti
+        corrupti doloribus voluptatum eligendi obcaecati odio numquam. Similique
+        porro voluptatem veniam.
+      </Card>
+      <Card imageSource={"./images/17.jpg"}>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Incidunt
+        placeat at vero, ipsum laudantium excepturi dolores similique tempora.
+      </Card>
+      <Card imageSource={"./images/18.jpg"}>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit, delectus.
+      </Card>
+      <Card imageSource={"./images/19.jpg"}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio delectus
+        et totam. Maiores, veritatis voluptatum?
+      </Card>
+      <Card imageSource={"./images/20.jpg"}>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repudiandae
+        dicta voluptatum tenetur officia dolore voluptates illum.
+      </Card>
+      <Card imageSource={"./images/21.jpg"}>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deserunt
+        architecto fugit dolores odit recusandae nisi, ipsam maiores atque.
+      </Card>
+      <Card imageSource={"./images/22.jpg"}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa assumenda
+        odio fugiat quos!
+      </Card>
+      <Card imageSource={"./images/23.jpg"}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic accusamus
+        vel rem. Cupiditate.
+      </Card>
+      <Card imageSource={"./images/24.jpg"}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam
+        aliquam, suscipit excepturi saepe porro officia magni fuga!
+      </Card>
+      <Card imageSource={"./images/25.jpg"}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore
+        aspernatur beatae cum laborum ut molestias labore. Dolorem neque
+        similique porro?
+      </Card>
+      <Card imageSource={"./images/26.jpg"}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut ab error
+        perspiciatis ipsum, quae eos.
+      </Card>
+      <Card imageSource={"./images/27.jpg"}>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Explicabo
+        ullam corporis ipsum harum animi architecto, id, quia dolorem quae
+        impedit sunt? Sunt ipsum dolores neque magni voluptatibus error
+        explicabo a?
+      </Card>
+    </div>
+  );
+};
+
+export default Gallery;
