f0ce0232bf1179cbd5ae6c23d09570253703b6ba
farkhanmhd <farkhanmuhammadv3@gmail.com>
2023-11-27 00:46:19 +0700

    fixed line height issues

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -6,7 +6,7 @@ export default function Card({ imageSource, children }) {
         alt={"image"}
         className="w-full h-auto grayscale-[.4] group-hover:grayscale-0 duration-300 group-hover:scale-110"
       />
-      <p className=" px-5 pb-2 leading-[28px] absolute bottom-0 opacity-0 group-hover:opacity-100 bg-gradient-to-b from-transparent to-slate-900 text-white duration-300 line-clamp-2 text-ellipsis">
+      <p className=" px-5 pb-2 leading-[29px] absolute bottom-0 opacity-0 group-hover:opacity-100 bg-gradient-to-b from-transparent to-slate-900 text-white duration-300 line-clamp-2 text-ellipsis">
         {children}
       </p>
     </div>
